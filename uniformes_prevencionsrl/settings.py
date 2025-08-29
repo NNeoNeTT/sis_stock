b3b8716f0bbc2b389f43617b7cb5f5c6e66e8eb1
Egidio De Jesus Sanchez Sanguinetti <wizardofdestiny9@gmail.com>
2025-08-29 15:00:23 -0300

    update

--- uniformes_prevencionsrl/settings.py
+++ uniformes_prevencionsrl/settings.py
@@ -1,13 +1,18 @@
-
-from pathlib import Path
 import os
+from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Seguridad
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-dev")
-DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
-ALLOWED_HOSTS = ["*"]
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
+# Hosts y CSRF
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
+
+# Aplicaciones
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -21,6 +26,7 @@ INSTALLED_APPS = [
     "apps.movimientos",
 ]
 
+# Middleware
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -33,6 +39,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "uniformes_prevencionsrl.urls"
 
+# Templates
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -51,32 +58,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "uniformes_prevencionsrl.wsgi.application"
 
+# Base de datos PostgreSQL (Railway DATABASE_URL)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR}/db.sqlite3", conn_max_age=600
+    )
 }
 
-ALLOWED_HOSTS = [
-    "sisstock-production.up.railway.app",
-    "127.0.0.1",  # para desarrollo local
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://sisstock-production.up.railway.app",
-]
-
-
+# Localización
 LANGUAGE_CODE = "es"
 TIME_ZONE = "America/Asuncion"
 USE_I18N = True
 USE_TZ = True
 
+# Archivos estáticos
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# Login
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
