eb228c586e36c3fb1b02e48b8b6b7ed7551d5557
Egidio De Jesus Sanchez Sanguinetti <wizardofdestiny9@gmail.com>
2025-08-29 14:38:42 -0300

    update

--- uniformes_prevencionsrl/settings.py
+++ uniformes_prevencionsrl/settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-dev")
-DEBUG = os.environ.get("DJANGO_DEBUG", "Flase") == "True"
+DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
